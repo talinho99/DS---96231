595fba168d0452a2e07ba474794bb68428c6c557
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-17 21:40:38 -0300

    back-end projeto fullstack

---
+++ DS-96231-X-DTO-CRIPTO/DS-96231-X-DTO-CRIPTO/projeto/src/main/java/com/example/projeto/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.projeto.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    // Permite o uso de criptografia na API.
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    // Define quem pode acessar a API.
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
